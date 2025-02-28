d201e52e1170b03dd0393554d550fd5ae9e6177c
codingstart0 <codingstart0@gmail.com>
2025-02-28 12:04:59 +0200

    Update TaskService to get data from server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
-import{ Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
 
+  constructor(private http: HttpClient) {}
+
+/*************  ✨ Codeium Command ⭐  *************/
+  /**
+   * Retrieves a list of tasks from the API.
+   *
+   * @returns an observable of the list of tasks
+   */
+/******  a597614f-a25e-4509-9096-a527d425b744  *******/
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS); 
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
